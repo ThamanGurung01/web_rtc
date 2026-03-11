7ded8181c63b7f61655b694927f1a7a597dd25b4
Thaman Gurung <tgurung519@gmail.com>
2026-03-11 22:00:24 +0545

    setup: signaling server

---
+++ signaling_server/server.js
@@ -0,0 +1,17 @@
+import {WebSocketServer} from "ws";
+const wss=new WebSocketServer({ port: 3000 });
+let clients=[];
+wss.on("connection",(ws)=>{
+    clients.push(ws);
+    ws.on("message",(message)=>{
+        clients.forEach(client=>{
+            if(client!==ws&&client.readyState===WebSocket.OPEN){
+                client.send(message.toString());
+            }
+        })
+    })
+    ws.on("close",()=>{
+        clients=clients.filter(client=>client!==ws);
+    })
+})
+console.log("WebSocket signaling server is running on ws://localhost:3000");
