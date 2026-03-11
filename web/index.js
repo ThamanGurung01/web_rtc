196d28c9676b9404bd750cbebfdd22cbe919c72d
Thaman Gurung <tgurung519@gmail.com>
2026-03-11 21:42:48 +0545

    feat: created Offer

---
+++ web/index.js
@@ -0,0 +1,64 @@
+const videoElem = document.getElementById("video");
+const offerElem=document.getElementById("offer");
+// const startElem = document.getElementById("start");
+// const stopElem = document.getElementById("stop");
+const displayMediaOptions = {
+  video: {
+    displaySurface: "browser",
+  },
+  audio: {
+    suppressLocalAudioPlayback: false,
+  },
+  preferCurrentTab: false,
+  selfBrowserSurface: "exclude",
+  systemAudio: "include",
+  surfaceSwitching: "include",
+  monitorTypeSurfaces: "include",
+};
+async function startCapture(displayMediaOptions) {
+  let captureStream = null;
+
+  try {
+    captureStream =
+      await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+  return captureStream;
+}
+
+async function createOffer(displayMediaOptions){
+  console.log("create offer....");
+const captureStream=await startCapture(displayMediaOptions);
+videoElem.srcObject = captureStream;
+videoElem.play();
+const pc=new RTCPeerConnection({
+  iceServers:[
+    {
+      urls:"stun:stun.l.google.com:19302"
+    }
+  ]
+});
+captureStream.getTracks().forEach(track=>{
+  pc.addTrack(track,captureStream);
+})
+const offer=await pc.createOffer();
+await pc.setLocalDescription(offer);
+console.log("offer",offer);
+}
+offerElem.addEventListener("click",async()=>{
+ await createOffer(displayMediaOptions);
+});
+
+// startElem.addEventListener("click", async () => {
+// console.log("start capture....");
+// const captureStream = await startCapture(displayMediaOptions);
+// console.log("captureStream", captureStream);
+// videoElem.srcObject = captureStream;
+// videoElem.play();
+// });
+// stopElem.addEventListener("click", () => {
+// let tracks=videoElem.srcObject.getTracks();
+// tracks.forEach((track) => track.stop());
+// videoElem.srcObject = null;
+// });
